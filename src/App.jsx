48019e6ae3a979adf619c62d80ae0e8c40422084
WithinKael <138517664+WithinKael@users.noreply.github.com>
2023-11-27 16:08:26 +0100

    add: deploy workflow

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
-import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="*" />
-      </Routes>
+      <Home></Home>
     </>
   );
 }
