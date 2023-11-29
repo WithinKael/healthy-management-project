afcb35c6fbb2b08204c2a26bcc5837a11b5dfa86
WithinKael <138517664+WithinKael@users.noreply.github.com>
2023-11-29 19:49:59 +0100

    add whyChooseSection

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
+import WhyChooseUs from "./components/WhyChooseUs/WhyChooseUs";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Header />
       <main>
         <Hero />
+        <WhyChooseUs />
       </main>
       <Footer />
     </>
