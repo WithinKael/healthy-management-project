36313c64939d684bfd124434a7bada6fb0120977
Valentan390 <137200405+Valentan390@users.noreply.github.com>
2023-11-29 19:59:10 +0200

    Add Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
+import AboutMe from "./components/AboutMe/AboutMe";
+import AboutStudio from "./components/AboutStudio/AboutStudio";
 import BusinessTools from "./components/BusinessTools/BusinessTools";
-import Footer from "./components/Footer/Footer";
+// import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 
@@ -9,9 +11,11 @@ function App() {
       <Header />
       <main>
         <Hero />
+        <AboutStudio />
+        <AboutMe />
         <BusinessTools />
       </main>
-      <Footer />
+      {/* <Footer /> */}
     </>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,80 @@
+import { useState } from "react";
+import s from "./Hedar.module.css";
+import logo from "../../images/sprite.svg";
+
 const Header = () => {
-  return <div className="container">Header</div>;
+  const [activeLink, setActiveLink] = useState("home");
+  console.log(activeLink);
+
+  const handleSetActiveLink = (link, event) => {
+    event.preventDefault();
+    console.log(`Setting active link to: ${link}`);
+    setActiveLink(link);
+  };
+
+  return (
+    <header className={`${s.header} ${s.fixedHeader}`}>
+      <div className={s.headerDiv}>
+        <a href="#home">
+          <svg width="227" height="40">
+            <use href={`${logo}#logo-black`}></use>
+          </svg>
+        </a>
+        <nav className={s.headerNav}>
+          <a
+            href="#home"
+            className={`${s.headerNavMenu} ${
+              activeLink === "home" ? s.active : ""
+            }`}
+            onClick={(e) => handleSetActiveLink("home", e)}
+          >
+            Головна
+          </a>
+          <a
+            href="#aboutUs"
+            className={`${s.headerNavMenu} ${
+              activeLink === "aboutUs" ? s.active : ""
+            }`}
+            onClick={(e) => handleSetActiveLink("aboutUs", e)}
+          >
+            Про нас
+          </a>
+          <a
+            href="#services"
+            className={`${s.headerNavMenu} ${
+              activeLink === "services" ? s.active : ""
+            }`}
+            onClick={(e) => handleSetActiveLink("services", e)}
+          >
+            Послуги
+          </a>
+          <a
+            href="#mentoring"
+            className={`${s.headerNavMenu} ${
+              activeLink === "mentoring" ? s.active : ""
+            }`}
+            onClick={(e) => handleSetActiveLink("mentoring", e)}
+          >
+            Менторство
+          </a>
+          <a
+            href="#projects"
+            className={`${s.headerNavMenu} ${
+              activeLink === "projects" ? s.active : ""
+            }`}
+            onClick={(e) => handleSetActiveLink("projects", e)}
+          >
+            Проєкти
+          </a>
+        </nav>
+        <address>
+          <a href="#contact" className={s.headerAddress}>
+            Контакт
+          </a>
+        </address>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
