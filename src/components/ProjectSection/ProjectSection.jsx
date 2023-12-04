0b744cfb554fde4901cf7082dfadcda18e5871aa
Vyachuk <Slavkoka045@gmail.com>
2023-12-04 21:33:10 +0200

    UPDATE: adaptive styles + function for more items visible in mobile version

--- src/components/ProjectSection/ProjectSection.jsx
+++ src/components/ProjectSection/ProjectSection.jsx
@@ -1,22 +1,57 @@
+import { useScreenWidth } from "../../hooks/useScreenWidth";
 import data from "./data";
+import icon from "../../images/sprite.svg";
 
-import { Title, List } from "./ProjectSection.styled";
+import {
+  Title,
+  List,
+  ActionBtn,
+  SectionWrapper,
+  Icon,
+} from "./ProjectSection.styled";
 import ProjectSectionCard from "./ProjectSectionCard/ProjectSectionCard";
+import { useState } from "react";
 
 const ProjectSection = () => {
+  const screenWidth = useScreenWidth();
+
+  const [projectList, setProjectList] = useState([...data.slice(0, 3)]);
+  const handleListChange = () => {
+    if (projectList.length > 4) {
+      setProjectList((prev) => prev.slice(0, 3));
+    } else {
+      setProjectList([...data]);
+    }
+  };
+
   return (
-    <section
+    <SectionWrapper
       id="projects"
       className="container"
       style={{ paddingTop: "80px", paddingBottom: "80px" }}
     >
       <Title>Проєкти</Title>
       <List>
-        {data.map((item) => (
+        {projectList.map((item) => (
           <ProjectSectionCard key={item.category} item={item} />
         ))}
       </List>
-    </section>
+      {screenWidth === "mobile" && (
+        <ActionBtn onClick={handleListChange}>
+          {projectList.length > 3 ? "Менше проєктів" : "Більше проєктів"}
+          <Icon>
+            <use
+              href={
+                icon +
+                `${
+                  projectList.length > 3 ? "#icon-arrowup" : "#icon-arrowdown"
+                }`
+              }
+            ></use>
+          </Icon>
+        </ActionBtn>
+      )}
+    </SectionWrapper>
   );
 };
 
--- src/components/ProjectSection/ProjectSection.styled.js
+++ src/components/ProjectSection/ProjectSection.styled.js
@@ -1,5 +1,14 @@
 import styled from "@emotion/styled";
 
+export const SectionWrapper = styled.section`
+  padding-top: 40px;
+  padding-bottom: 40px;
+  @media screen and (min-width: 768px) {
+    padding-top: 80px;
+    padding-bottom: 80px;
+  }
+`;
+
 export const Title = styled.h2`
   font-size: 48px;
   font-weight: 600;
@@ -14,4 +23,26 @@ export const List = styled.ul`
   flex-wrap: wrap;
   @media screen and (min-width: 768px) {
   }
+`;
+
+export const ActionBtn = styled.span`
+  font-size: 14px;
+  font-weight: 600;
+  line-height: calc(18 / 14);
+  margin-top: 4px;
+  padding-top: 20px;
+
+  display: flex;
+  gap: 14px;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+`;
+
+export const Icon = styled.svg`
+  fill: currentColor;
+  stroke: currentColor;
+
+  width: 24px;
+  height: 24px;
 `;
