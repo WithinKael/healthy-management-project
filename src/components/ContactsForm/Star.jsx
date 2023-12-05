45d7de7ea00a17b19fcc52a531629b86051fcc75
Aleksandr2905 <natalukha@ukr.net>
2023-12-05 22:06:02 +0200

    styled form

---
+++ src/components/ContactsForm/Star.jsx
@@ -0,0 +1,13 @@
+import sprite from "../../images/icon-footer.svg";
+
+const Star = () => {
+  return (
+    <svg width="8" height="8">
+      <use href={`${sprite}#star`} />
+    </svg>
+  );
+};
+
+export default Star;
+
+// style={{ verticalAlign: "super" }}
