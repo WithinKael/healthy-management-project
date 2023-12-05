c3f8573bf38be678022ed9815a432c13d7d58eed
WithinKael <138517664+WithinKael@users.noreply.github.com>
2023-12-05 19:28:59 +0100

    add 375px whychoose

--- src/components/WhyChooseUs/WhyChooseUs.styled.js
+++ src/components/WhyChooseUs/WhyChooseUs.styled.js
@@ -1,26 +1,38 @@
 import styled from "@emotion/styled";
 
-
 export const SectionChoose = styled.section`
-  padding-top: 80px;
-  padding-bottom: 80px;
-`
+  padding-top: 24px;
+  padding-bottom: 24px;
+
+  @media screen and (min-width: 1920px) {
+    padding-top: 80px;
+    padding-bottom: 80px;
+  }
+`;
 
 export const ContentWrapper = styled.div`
-  max-width: 100%;
-  height: 680px;
   border-radius: 16px;
-  padding: 80px;
+  padding: 24px;
   background: var(--background-bluedark);
+
+  @media screen and (min-width: 1920px) {
+    max-width: 100%;
+    padding: 80px;
+  }
 `;
 
 export const ContentTitle = styled.h2`
-  font-size: 48px;
-  font-style: normal;
-  font-weight: 600;
-  line-height: 56px;
   color: var(--system-default-10);
-  margin-bottom: 40px;
+  font-size: 24px;
+  font-weight: 600;
+  line-height: 32px;
+  margin-bottom: 24px;
+
+  @media screen and (min-width: 1920px) {
+    font-size: 48px;
+    line-height: 56px;
+    margin-bottom: 40px;
+  }
 `;
 
 export const List = styled.ul`
@@ -32,11 +44,17 @@ export const List = styled.ul`
 
 export const Item = styled.li`
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
-  column-gap: 24px;
-  flex-basis: calc((100% - 24px) / 2);
-  padding: 40px;
+  gap: 16px;
+
+  @media screen and (min-width: 1920px) {
+    flex-direction: row;
+    flex-basis: calc((100% - 24px) / 2);
+    padding: 40px;
+    gap: 24px;
+  }
 `;
 
 export const Svg = styled.svg`
@@ -47,20 +65,34 @@ export const Svg = styled.svg`
 export const Div = styled.div`
   display: flex;
   flex-direction: column;
-`
+`;
 
 export const ItemTitle = styled.h3`
-  color: var(--system-default-10);
-  font-size: 24px;
+  width: 100%;
   font-weight: 600;
-  line-height: 32px;
-  margin-bottom: 16px;
+  font-size: 18px;
+  line-height: 24px;
+  margin-bottom: 8px;
+  color: var(--system-default-10);
+
+  @media screen and (min-width: 1920px) {
+    font-size: 24px;
+    line-height: 32px;
+    margin-bottom: 16px;
+  }
 `;
 
 export const Text = styled.p`
-  max-width: 542px;
   color: var(--system-default-10);
-  font-size: 16px;
+  font-size: 14px;
   font-weight: 500;
-  line-height: 24px;
+  line-height: 20px;
+  font-style: normal;
+  font-family: Montserrat;
+
+  @media screen and (min-width: 1920px) {
+    max-width: 542px;
+    font-size: 16px;
+    line-height: 24px;
+  }
 `;
