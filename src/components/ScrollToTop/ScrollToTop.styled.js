55fcc0a3488a408c955a51d1325d001324e263ec
Aleksandr2905 <natalukha@ukr.net>
2023-12-11 23:25:48 +0200

    button scroll to top adaptive

--- src/components/ScrollToTop/ScrollToTop.styled.js
+++ src/components/ScrollToTop/ScrollToTop.styled.js
@@ -4,13 +4,33 @@ export const ScrollToTopButton = styled.button`
   display: flex;
   align-items: center;
   justify-content: center;
-  width: 56px;
-  height: 56px;
+  width: 44px;
+  height: 44px;
   position: fixed;
   bottom: var(--scroll-to-top-bottom);
-  right: 120px;
+  right: 40px;
   padding: 8px;
   background-color: var(--primary-yellow);
+  box-shadow: 0px 0px 8px 0px rgba(27, 54, 65, 0.08);
   border: none;
   border-radius: 16px;
+  z-index: 999;
+
+  &:hover {
+    box-shadow: 0px 0px 16px 0px rgba(27, 54, 65, 0.16);
+  }
+
+  @media screen and (min-width: 768px) {
+    right: 80px;
+  }
+
+  @media screen and (min-width: 1440px) {
+    width: 56px;
+    height: 56px;
+    right: 80px;
+  }
+
+  @media screen and (min-width: 1920px) {
+    right: 120px;
+  }
 `;
