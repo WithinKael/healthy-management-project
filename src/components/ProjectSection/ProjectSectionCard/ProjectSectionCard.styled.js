c1ece1a6484a53bf61c40cc9b05ce037b466fd1a
Vyachuk <Slavkoka045@gmail.com>
2023-12-13 23:11:36 +0200

    ADD: adaptive to Project Card

--- src/components/ProjectSection/ProjectSectionCard/ProjectSectionCard.styled.js
+++ src/components/ProjectSection/ProjectSectionCard/ProjectSectionCard.styled.js
@@ -3,7 +3,7 @@ import styled from "@emotion/styled";
 export const ListItem = styled.li`
   width: 100%;
   padding: 24px;
-  border-radius: 8px;
+  border-radius: 16px;
   height: 772px;
 
   box-shadow: 0px 0px 8px 0px rgba(27, 54, 65, 0.08);
@@ -12,14 +12,16 @@ export const ListItem = styled.li`
   overflow: hidden;
   position: relative;
   @media screen and (min-width: 768px) {
-    width: calc((100% - (2 * 24px)) / 3);
     padding: 40px;
-    border-radius: 16px;
-    height: 808px;
+    height: 676px;
 
     &:hover > div {
       opacity: 1;
     }
+  }
+  @media screen and (min-width: 1440px) {
+    width: calc((100% - 2 * 24px) / 3);
+    height: 808px;
   }
 `;
 
